32415c54edee3cf62414cb762e019b02a4a7bcb5
khadkaraghav <khadkaraghav@gmail.com>
2019-02-28 11:15:01 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Spring17SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring17SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring17SecurityApplication.class, args);
+    }
+
+}
